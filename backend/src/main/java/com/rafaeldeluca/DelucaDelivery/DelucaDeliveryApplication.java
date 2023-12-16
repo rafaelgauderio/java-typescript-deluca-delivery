c5b1887cd24b14eb48a77e9c62a02575a92a0694
Rafael de Luca <deluca1712@gmail.com>
2023-12-16 19:34:37 -0300

    Create project

---
+++ backend/src/main/java/com/rafaeldeluca/DelucaDelivery/DelucaDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.rafaeldeluca.DelucaDelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelucaDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelucaDeliveryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rafaeldeluca/DelucaDelivery/DelucaDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaeldeluca.DelucaDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelucaDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
