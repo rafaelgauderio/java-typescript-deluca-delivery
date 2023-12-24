0ff985d5854a7633113757cbcb82fee87c763b5a
Rafael de Luca <deluca1712@gmail.com>
2023-12-24 20:07:53 -0300

    h2 database initial seed

--- backend/src/main/java/com/rafaeldeluca/DelucaDelivery/config/ResourceServerConfig.java
+++ backend/src/main/java/com/rafaeldeluca/DelucaDelivery/config/ResourceServerConfig.java
@@ -1,10 +1,15 @@
 package com.rafaeldeluca.DelucaDelivery.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class ResourceServerConfig {
@@ -20,5 +25,14 @@ public class ResourceServerConfig {
                         .ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")));
         return http.build();
     }
+    
+    @Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
 
 }
